a1da1279e026e07fc6ea8b7f7fa37f9e65d6e6a8
KirtiJha <kirti.jha2025@gmail.com>
2025-03-17 20:19:25 +0530

    adding some js features

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-let elem1 = document.querySelector("#elem1");
+// this is only for 1st img...or 1st div...for try purpose
 
-let elemImg = document.querySelector("#elem1 img");
+// let elem1 = document.querySelector("#elem1");
 
-elem1.addEventListener("mousemove", (dets) => {
-    elemImg.style.left = dets.x+"px";
-    elemImg.style.top = dets.y+"px";
-});
+// let elemImg = document.querySelector("#elem1 img");
 
-elem1.addEventListener("mouseenter", (dets) => {
-    elemImg.style.opacity = 1; 
-});
+// elem1.addEventListener("mousemove", (dets) => {
+//     elemImg.style.left = dets.x+"px";
+//     elemImg.style.top = dets.y+"px";
+// });
 
-elem1.addEventListener("mouseleave", (dets) => {
-    elemImg.style.opacity = 0; 
+// elem1.addEventListener("mouseenter", (dets) => {
+//     elemImg.style.opacity = 1;
+// });
+
+// elem1.addEventListener("mouseleave", (dets) => {
+//     elemImg.style.opacity = 0;
+// });
+
+// this is the final for all img...and all div...
+let elem = document.querySelectorAll(".elem");
+
+elem.forEach((value) => {
+  value.addEventListener("mouseenter", () => {
+    value.childNodes[3].style.opacity = 1;
+  });
+
+  value.addEventListener("mouseleave", () => {
+    value.childNodes[3].style.opacity = 0;
+  });
+
+  value.addEventListener("mousemove", (dets) => {
+    value.childNodes[3].style.left = dets.x + "px";
+    value.childNodes[3].style.top = dets.y + "px";
+  });
 });
